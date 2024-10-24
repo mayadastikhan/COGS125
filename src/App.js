4a35c90b16dec6f7e9e4c7d540a1480f0b51c76c
mayadastikhan <mayadastikhan@gmail.com>
2024-10-24 16:05:35 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import image from "./pngtree-user-experience-png-image_6290263.png"
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="App-header">
+      <h1>Maya Khan</h1>
+      <h2>Interaction Design</h2>
+      <img src={image} className="App-logo" alt="logo" />
+      <a
+        className="Project1-Link"
+        href="https://www.figma.com/design/bqaCYYtnmjIh4E3y9lKfso/Project-1%3A-Inch%2C-Foot%2C-Yard-Scale?m=dev&t=Uo9qonkwnPxUwE6L-1"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ color: 'white' }}  // Keeps the link text white
+      >
+        Project 1: Inch, Foot, Yard Scale
+      </a>
+    </header>
+  </div>
+);
 }
 
 export default App;
