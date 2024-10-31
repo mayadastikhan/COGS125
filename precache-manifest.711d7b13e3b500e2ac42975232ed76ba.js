dcbda51142f4984900065f065e2935c9e01166f2
mayadastikhan <mayadastikhan@gmail.com>
2024-10-31 10:31:24 -0700

    Updates

---
+++ precache-manifest.711d7b13e3b500e2ac42975232ed76ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d699a2f920170a149f810ec1dca93602",
+    "url": "/COGS125/index.html"
+  },
+  {
+    "revision": "e1e572bc054f0273b96e",
+    "url": "/COGS125/static/css/main.1eb42af0.chunk.css"
+  },
+  {
+    "revision": "bda4530cfcea0dc53868",
+    "url": "/COGS125/static/js/2.3f0f6595.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/COGS125/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "e1e572bc054f0273b96e",
+    "url": "/COGS125/static/js/main.b1197546.chunk.js"
+  },
+  {
+    "revision": "6cafdc7cae325ede5fe0",
+    "url": "/COGS125/static/js/runtime~main.94ad87e4.js"
+  }
+]);
